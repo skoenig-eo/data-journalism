71eec6f94b1d081da8e52ebd9c93dc6fee880607
Simon König <simonkoenig@live.de>
2022-10-06 00:03:32 +0200

    add comments

--- allesgesagt/plot_allesgesagt_01.R
+++ allesgesagt/plot_allesgesagt_01.R
@@ -1,3 +1,22 @@
+# Header --------------------------------------------
+#
+# Original Author: Simon König, simonkoenig@live.de
+# Created on: 2022-03-11
+# 
+# Last modified by: Simon König, simonkoenig@live.de
+# On: 2022-10-05
+#
+# Script Description: some nice plots of the allesgesagt data
+# 
+# Notes: 
+#
+#
+#
+
+
+
+# load libraries and raw data --------------------------------------
+
 library(lubridate)
 library(cowplot)
 library(tidyverse)
@@ -6,11 +25,16 @@ library(tidyverse)
 allesgesagt_raw <- read_csv("allesgesagt/allesgesagt_raw.csv")
 # fix(allesgesagt_raw)
 
+# a little bit of data wrangling
 allesgesagt <- allesgesagt_raw %>% 
   mutate(Date = make_date(year, month, day),
          Duration = hours*60+minutes)
 
 
+
+# first plots ------------------------
+
+# plot time series of total interview length
 ggplot(allesgesagt) +
   geom_smooth(aes(x = Date, y = Duration), se = FALSE, alpha = 0.5,
               color = "darkblue", size = 0.5) +
@@ -29,6 +53,7 @@ ggplot(allesgesagt) +
 ggsave("allesgesagt/duration_ts_01.png",
        width = 18, height = 12, units = "cm", dpi = 450)
 
+# compare length distributions for male vs. female guests
 ggplot(allesgesagt) +
   geom_boxplot(aes(x = gender, y = Duration), width = 0.5) +
   scale_y_continuous(expand = c(0,0), limits = c(0, 600)) +
