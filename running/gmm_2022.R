51f8e4c402b3b7491542f1fd75ba06a8a559386b
Simon König <simonkoenig@live.de>
2022-11-24 17:38:21 +0100

    add running time stats

--- running/gmm_2022.R
+++ running/gmm_2022.R
@@ -28,7 +28,7 @@ results <- read_excel("running/ErgebnislistenErgebnislisteOA.xlsx",
   drop_na() %>% 
   rename(ID = 1, Number = 2, Name = 3, Nationality = 4, Team = 5, Birthyear = 6,
          Group = 7, Place = 8, Gross = 9, Net = 10, Difference = 11) %>% 
-  select(Number, Nationality, Birthyear, Group, Place, Gross, Net) %>% 
+  select(-ID, -Team) %>% 
   # running times are pasted as fractions of a day for some reason
   mutate(Net_seconds = Net*60*60*24,
          Rounded_seconds = floor(Net_seconds),
@@ -38,5 +38,21 @@ results <- read_excel("running/ErgebnislistenErgebnislisteOA.xlsx",
          Seconds = second(Net_approx),
          In_minutes = Hours*60+Minutes+Seconds/60)
 
+# create some summary statistics
+median_time <- median(results$In_minutes)
+perc_01 <- quantile(results$In_minutes, 0.01)
+perc_10 <- quantile(results$In_minutes, 0.1)
+perc_25 <- quantile(results$In_minutes, 0.25)
+
+# check out my position
+my_result <- results %>% filter(Name == "KÖNIG Simon")
+
+
 ggplot(results) +
-  geom_density(aes(x = In_minutes))
+  geom_density(aes(x = In_minutes)) +
+  geom_vline(xintercept = median_time) +
+  geom_vline(xintercept = perc_01) +
+  geom_vline(xintercept = perc_10) +
+  geom_vline(xintercept = perc_25) +
+  geom_vline(xintercept = my_result %>% pull(In_minutes)) +
+  theme_half_open()
