d633395a51c3e111809ca678696b60e2891ff831
Simon König <simonkoenig@live.de>
2022-11-26 23:54:59 +0100

    further improve plot styling

--- running/gmm_2022.R
+++ running/gmm_2022.R
@@ -49,10 +49,15 @@ my_result <- results %>% filter(Name == "KÖNIG Simon")
 
 
 ggplot(results) +
-  geom_density(aes(x = In_minutes)) +
-  geom_vline(xintercept = median_time) +
-  geom_vline(xintercept = perc_01) +
-  geom_vline(xintercept = perc_10) +
-  geom_vline(xintercept = perc_25) +
-  geom_vline(xintercept = my_result %>% pull(In_minutes)) +
-  theme_half_open()
+  geom_density(aes(x = In_minutes), color = "grey20",
+               fill = "lightgrey", alpha = 0.25) +
+  geom_vline(xintercept = median_time, linetype = "dashed") +
+  geom_vline(xintercept = perc_01, linetype = "dotted") +
+  geom_vline(xintercept = perc_10, color = "#C11C03", size = 1) +
+  geom_vline(xintercept = perc_25, linetype = "dotted") +
+  geom_vline(xintercept = my_result %>% pull(In_minutes),
+             color = "#8E1230") +
+  scale_x_continuous(expand = c(0,0), name = "Running Time [minutes]") +
+  scale_y_continuous(expand = c(0,0), name = "Density") +
+  theme_half_open() +
+  ggtitle("GMM Half Marathon Results 2022")
