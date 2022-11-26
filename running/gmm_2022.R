dc361513add570916d79843c660be854fb1345d5
Simon König <simonkoenig@live.de>
2022-11-27 00:52:22 +0100

    restructure whole script

--- running/gmm_2022.R
+++ running/gmm_2022.R
@@ -1,11 +1,31 @@
+# Header --------------------------------------------
+#
+# Original Author: Simon König, simonkoenig@live.de
+# Created on: 2022-03-11
+# 
+# Last modified by: Simon König, simonkoenig@live.de
+# On: 2022-10-27
+#
+# Script Description: plot GMM 2022 finisher data for the half marathon
+# and find a suitable finishing time for 2023
+# 
+# Notes: 
+#
+#
+#
+
 library(readxl)
 library(lubridate)
 library(cowplot)
 library(tidyverse)
 
-raw_results <- read_excel("running/ErgebnislistenErgebnislisteOA.xlsx")
-names(raw_results)
-raw_results
+
+
+# load and wrangle data ----------------------------------------------
+
+# raw_results <- read_excel("running/ErgebnislistenErgebnislisteOA.xlsx")
+# names(raw_results)
+# raw_results
 
 # column types
 #"Platz...1" integer 
@@ -38,15 +58,21 @@ results <- read_excel("running/ErgebnislistenErgebnislisteOA.xlsx",
          Seconds = second(Net_approx),
          In_minutes = Hours*60+Minutes+Seconds/60)
 
-# create some summary statistics
+
+
+# create some summary statistics -----------------------------------
+
 median_time <- median(results$In_minutes)
 perc_01 <- quantile(results$In_minutes, 0.01)
 perc_10 <- quantile(results$In_minutes, 0.1)
 perc_25 <- quantile(results$In_minutes, 0.25)
 
-# check out my position
+# check out my position in 2022
 my_result <- results %>% filter(Name == "KÖNIG Simon")
 
+
+
+# basic plot version ----------------------------------------------
 
 ggplot(results) +
   geom_density(aes(x = In_minutes), color = "grey20",
@@ -61,3 +87,22 @@ ggplot(results) +
   scale_y_continuous(expand = c(0,0), name = "Density") +
   theme_half_open() +
   ggtitle("GMM Half Marathon Results 2022")
+
+
+
+# fancy plot: overall finishing times plus target for 2023 --------------------
+
+# slightly changed from base plot
+ggplot(results) +
+  geom_density(aes(x = In_minutes), color = NA,
+               fill = "lightgrey", alpha = 0.6) +
+  geom_vline(xintercept = median_time, linetype = "dashed") +
+  geom_vline(xintercept = perc_01, linetype = "dotted") +
+  geom_vline(xintercept = perc_10, color = "#C11C03", size = 1) +
+  geom_vline(xintercept = perc_25, linetype = "dotted") +
+  geom_vline(xintercept = my_result %>% pull(In_minutes),
+             color = "#8E1230") +
+  scale_x_continuous(expand = c(0,0), name = "Running Time [minutes]") +
+  scale_y_continuous(expand = c(0,0), name = "Density") +
+  theme_half_open() +
+  ggtitle("GMM Target Time for 2023")
